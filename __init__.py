1dc4a4598ad7e1bfb703883456a4b927dccbe453
Benjamin Riggs <deisum@gmail.com>
2014-07-09 03:20:30 -0500

    Playing with the algorithm.

--- __init__.py
+++ __init__.py
@@ -7,16 +7,20 @@ from .analysis import running_average_coro, fft_power
 from .output import output
 
 from operator import itemgetter
+from random import choice
 #from time import sleep
+from numpy import subtract
 
 
 IP_ADDRESS = '10.1.10.25'
 PORT = 8844
 
-EXPONENT = 1.1
+EXPONENT = 1
 
 
 def main(data_file=None, debug=False):
+
+    print("\n     {0}\n".format(choice(("Right", "Left"))))
 
     if debug:  # Log to stdout
         import logging, sys
@@ -36,31 +40,36 @@ def main(data_file=None, debug=False):
     P4_displacement = 0
     C4_displacement = 0
 
-    P4_velocity = 3
-    C4_velocity = 3
+    P4_velocity = 1
+    C4_velocity = 1
 
     total_displacement = 0
 
     cap.acquire_data(0.5)  # Make sure there's enough data for first analysis
 
-    while -45 < total_displacement < 45:
+    while True:
 
-        output(total_displacement)
+        #output(total_displacement)
+        print(total_displacement)
 
         cap.acquire_data()
 
         # Because of 'data_age=0.5', only last 1/2 second of signal is analyzed
-        P4_power = fft_power(cap.sensor_data['P4'], sample_frequency=cap.sample_frequency)
-        C4_power = fft_power(cap.sensor_data['C4'], sample_frequency=cap.sample_frequency)
+        Fz_power = fft_power(cap.sensor_data['Fz'], sample_frequency=cap.sample_frequency)
+        C4_power = fft_power(cap.sensor_data['C3'], sample_frequency=cap.sample_frequency)
 
         # Get the max signal between 10 & 14 Hz
-        P4_signal = max(filter(lambda x: 10 < x[0] < 14, P4_power), key=itemgetter(1))[1]
-        C4_signal = max(filter(lambda x: 10 < x[0] < 14, C4_power), key=itemgetter(1))[1]
+        Fz_signal = max(filter(lambda x: 11 < x[0] < 13, Fz_power), key=itemgetter(1))[1]
+        C4_signal = max(filter(lambda x: 11 < x[0] < 13, C4_power), key=itemgetter(1))[1]
 
-        P4_average = P4_running_average.send(P4_signal)
+        """
+        total_displacement = Fz_signal - C4_signal
+        """
+
+        P4_average = P4_running_average.send(Fz_signal)
         C4_average = C4_running_average.send(C4_signal)
 
-        if P4_signal >= P4_average:
+        if Fz_signal >= P4_average:
             P4_displacement += P4_velocity
             P4_velocity *= EXPONENT
 
